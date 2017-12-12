03e5f84008540b43997dc213251e288116b45e94
pozes <pozes@DESKTOP-LKTIO2P>
2017-12-12 20:54:58 +0100

    a

--- JavaSynthesizer/src/view/Keyboard.java
+++ JavaSynthesizer/src/view/Keyboard.java
@@ -7,7 +7,7 @@ public class Keyboard extends AnchorPane {
 	private BDirka A0,B0;
 	private CDirka AH0; 					//nulta oktava
 	private BDirka C1,D1,E1,F1,G1,A1,B1;
-	private CDirka CH1,DH1,FH1,GH1,AH1; 	//prva oktavica
+	private CDirka CH1,DH1,FH1,GH1,AH1; 	//prva oktavicaa
 	
 	public Keyboard(){
 	}
