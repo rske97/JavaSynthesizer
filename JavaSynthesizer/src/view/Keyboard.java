86d0829e6213b6f82a3dea9676469e57ac3d1368
pozes <pozes@DESKTOP-LKTIO2P>
2017-12-12 20:03:04 +0100

    key

--- JavaSynthesizer/src/view/Keyboard.java
+++ JavaSynthesizer/src/view/Keyboard.java
@@ -1,6 +1,9 @@
 package view;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
-public class Keyboard {
-	
+public class Keyboard extends AnchorPane {
+	private Button A0,B0; //nulta oktava
+	private Button C1; //prva oktava
 }
